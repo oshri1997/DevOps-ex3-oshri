fe36281c263e2c50678e6bd74383df15e20b69d7
oshri1997 <oshri19970@gmail.com>
2023-07-18 13:08:23 +0300

    chang 1

--- Oshri.java
+++ Oshri.java
@@ -1,8 +1,8 @@
 public class Oshri {
 public static void main (String[] args){
-System.out.println("Oshri");
-System.out.println("Oshri");
-System.out.println("Oshri");
-System.out.println("Oshri");
+System.out.println("Oshri!");
+System.out.println("Oshri!");
+System.out.println("Oshri!");
+System.out.println("Oshri!");
 }
 }
