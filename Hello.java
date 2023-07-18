aa1283a741eecd953bd67000cc462d3d02c2e343
oshri1997 <oshri19970@gmail.com>
2023-07-18 13:19:11 +0300

    chang 5

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 public static void main (String[] args){
 for(int i=1;i<=5;i++){
-System.out.println("Hello world Hello Hello "+i);
+System.out.println("Hello world Hello DevOps "+i);
 }
 }
 }
